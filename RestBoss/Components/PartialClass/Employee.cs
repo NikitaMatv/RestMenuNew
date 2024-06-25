5c34c029a0f67588dcfdf27e63627cdc6c26b30c
NikitaMatv <kuzmenko_ka@mail.ru>
2024-06-25 09:57:37 +0300

    last

--- RestBoss/Components/PartialClass/Employee.cs
+++ RestBoss/Components/PartialClass/Employee.cs
@@ -8,11 +8,37 @@ namespace RestBoss.Components
 {
     public partial class Employee
     {
-       public TimeSpan? staj
+       public string staj
         {
             get
             {
-               return   DateTime.Now - DateSt;
+                //int years = DateTime.Now.Year - DateSt.Value.Month;
+
+                //if (DateTime.Now.Month < DateSt.Value.Month || (DateTime.Now.Month == DateSt.Value.Month && DateTime.Now.Day < DateSt.Value.Day))
+                //{
+                //    years--;
+                //}
+
+                //return years;
+                int years = DateTime.Now.Year - DateSt.Value.Year;
+                int months = DateTime.Now.Month - DateSt.Value.Month;
+
+                // Корректируем количество лет и месяцев, если текущий месяц и день меньше месяца и дня устройства
+                if (DateTime.Now.Day < DateSt.Value.Day)
+                {
+                    months--;
+                    if (months < 0)
+                    {
+                        years--;
+                        months += 12;
+                    }
+                }
+                else if (months < 0)
+                {
+                    years--;
+                    months += 12;
+                }
+                return $"{years} лет, {months} мес.";
             }
         }
     }
